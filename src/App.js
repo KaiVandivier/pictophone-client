b579c6759137ce297f12e0cecfce724c3a8a045f
Kai Vandivier <kpvandivier@gmail.com>
2020-04-27 21:33:25 -0700

    Add Canvas to test and config vars

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import socketIOClient from "socket.io-client";
-import './App.css';
+import { endpoint, prodEndpoint } from "./config";
+import Canvas from "./components/Canvas";
+import "./App.css";
 
-const ENDPOINT = "http://localhost:4000";
+const ENDPOINT =
+  process.env.NODE_ENV === "production" ? prodEndpoint : endpoint;
 
 function App() {
-  const [ message, setMessage ] = useState("Trying to connect...");
+  const [message, setMessage] = useState("Trying to connect...");
 
   useEffect(() => {
     const socket = socketIOClient(ENDPOINT);
     socket.on("message", (data) => {
       console.log(data);
       setMessage(data.message);
-    })
+    });
   }, []); // The empty array argument prevents this from running multiple times (which creates mulltiple sockets)
 
   return (
-    <h1>
-      {message}
-    </h1>
+  <div>
+    <h1>{message}</h1>
+    <Canvas />
+  </div>
   );
 }
 
