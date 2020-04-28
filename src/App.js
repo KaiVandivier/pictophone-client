258f8158befe09d92524bd1d39c2a225bd7fdb74
Kai Vandivier <kpvandivier@gmail.com>
2020-04-28 14:33:13 -0700

    Test a timer

--- src/App.js
+++ src/App.js
@@ -9,20 +9,35 @@ const ENDPOINT =
 
 function App() {
   const [message, setMessage] = useState("Trying to connect...");
+  const [secs, setSecs] = useState(0);
+  const [socket, setSocket] = useState(null);
 
   useEffect(() => {
-    const socket = socketIOClient(ENDPOINT);
-    socket.on("message", (data) => {
+    const newSocket = socketIOClient(ENDPOINT);
+    newSocket.on("message", (data) => {
       console.log(data);
       setMessage(data.message);
     });
-  }, []); // The empty array argument prevents this from running multiple times (which creates mulltiple sockets)
+    newSocket.on("time", (data) => {
+      setSecs(data.time);
+    });
+    setSocket(newSocket);
+  }, []);
+  // The empty array argument prevents this from unduly running multiple times
+
+  function startTimer() {
+    console.log(socket);
+    socket.emit("start-timer");
+    console.log("start-timer event sent");
+  }
 
   return (
-  <div>
-    <h1>{message}</h1>
-    <Canvas />
-  </div>
+    <div>
+      <h1>{message}</h1>
+      <button onClick={startTimer}>Start timer</button>
+      <h2>Time: {secs}</h2>
+      <Canvas />
+    </div>
   );
 }
 
