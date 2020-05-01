839f8e5dc4e3140978c4bac541546e2e807065f2
Kai Vandivier <kpvandivier@gmail.com>
2020-05-01 15:50:43 -0700

    Handle basic game loop

--- src/App.js
+++ src/App.js
@@ -1,43 +1,73 @@
 import React, { useState, useEffect } from "react";
 import socketIOClient from "socket.io-client";
 import { endpoint, prodEndpoint } from "./config";
-import Canvas from "./components/Canvas";
-import "./App.css";
+import Waiting from "./components/Waiting";
+import Drawing from "./components/Drawing";
+import WordChoosing from "./components/WordChoosing";
+import Guessing from "./components/Guessing";
 
 const ENDPOINT =
   process.env.NODE_ENV === "production" ? prodEndpoint : endpoint;
+const socket = socketIOClient(ENDPOINT);
+export const SocketContext = React.createContext(socket);
 
 function App() {
-  const [message, setMessage] = useState("Trying to connect...");
   const [secs, setSecs] = useState(0);
-  const [socket, setSocket] = useState(null);
+  const [phaseComponent, setPhaseComponent] = useState(
+    <Waiting onClick={sendReady} />
+  );
 
   useEffect(() => {
-    const newSocket = socketIOClient(ENDPOINT);
-    newSocket.on("message", (data) => {
-      console.log(data);
-      setMessage(data.message);
-    });
-    newSocket.on("time", (data) => {
+    socket.on("time", (data) => {
       setSecs(data.time);
     });
-    setSocket(newSocket);
   }, []);
   // The empty array argument prevents this from unduly running multiple times
 
-  function startTimer() {
-    console.log(socket);
-    socket.emit("start-timer");
-    console.log("start-timer event sent");
+  function sendReady() {
+    socket.once("choose-word", loadWordChoosing);
+    socket.emit("ready");
+  }
+
+  function loadWordChoosing({ words }) {
+    socket.once("load-drawing-phase", loadDrawingPhase);
+    setPhaseComponent(
+      <WordChoosing
+        words={words}
+        submitFn={(word) => socket.emit("word-chosen", { word })}
+      />
+    );
+  }
+
+  function loadDrawingPhase({ word }) {
+    // Note that there is already a time handler from `useEffect`
+    // socket.on("time"); // TODO: Handle time; maybe pass to Drawing
+    socket.once("load-guessing-phase", loadGuessingPhase);
+
+    setPhaseComponent(<Drawing word={word} />);
+  }
+
+  function loadGuessingPhase({ dataURL }) {
+    // TODO: Handle socket.on("time")
+    if (true) {
+      // TODO: Listen for "replay" too
+      socket.once("load-drawing-phase", loadDrawingPhase);
+    } else {
+      socket.once("load-replay", loadReplay);
+    }
+    setPhaseComponent(<Guessing dataURL={dataURL} />);
+  }
+
+  function loadReplay() {
+    setPhaseComponent(<h1>Hooray! Game cycle done!</h1>);
   }
 
   return (
-    <div>
-      <h1>{message}</h1>
-      <button onClick={startTimer}>Start timer</button>
-      <h2>Time: {secs}</h2>
-      <Canvas />
-    </div>
+    <SocketContext.Provider value={socket}>
+      <p>Time: {secs}</p>
+
+      {phaseComponent}
+    </SocketContext.Provider>
   );
 }
 
