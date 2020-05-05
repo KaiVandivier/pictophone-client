229b6280915ded59599a2fd7250dad951d20ad29
Kai Vandivier <kpvandivier@gmail.com>
2020-05-04 21:15:02 -0700

    Add some new messages from server

--- src/lib/messages.js
+++ src/lib/messages.js
@@ -1,22 +1,25 @@
-const messages = {
-  ready: "ready",
-  loadWordChoosing: "choose-word",
-  wordChoice: "word-chosen",
-  loadDrawingPhase: "load-drawing-phase",
-  drawingPhaseLoaded: "drawing-phase-loaded",
-  time: "time",
-  drawingTimeUp: "drawing-time-up",
-  imageData: "image-data",
-  loadGuessingPhase: "load-guessing-phase",
-  guessingPhaseLoaded: "guessing-phase-loaded",
-  guessingTimeUp: "guessing-time-up",
-  guessData: "guess-data",
-  loadReplay: "load-replay",
-};
+const msgs = Object.freeze({
+  TOGGLE_READY: "toggle-ready",
+  START_GAME: "start-game",
+
+  // ready: "ready",
+  // loadWordChoosing: "choose-word",
+  // wordChoice: "word-chosen",
+  // loadDrawingPhase: "load-drawing-phase",
+  // drawingPhaseLoaded: "drawing-phase-loaded",
+  // time: "time",
+  // drawingTimeUp: "drawing-time-up",
+  // imageData: "image-data",
+  // loadGuessingPhase: "load-guessing-phase",
+  // guessingPhaseLoaded: "guessing-phase-loaded",
+  // guessingTimeUp: "guessing-time-up",
+  // guessData: "guess-data",
+  // loadReplay: "load-replay",
+});
 
 export function checkSchemaMatch(serverSchema) {
   // TODO: check LENGTH and throw errors
-  return messages.keys.every((key) => serverSchema[key] === messages[key]);
+  return msgs.keys.every((key) => serverSchema[key] === msgs[key]);
 }
 
-export default messages;
+export default msgs;
