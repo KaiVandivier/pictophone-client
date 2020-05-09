ad833d722261c563183b04c72010b86fde419d40
Kai Vandivier <kpvandivier@gmail.com>
2020-05-09 10:04:58 -0700

    Remove duplicate

--- src/components/WordChoosing.js
+++ src/components/WordChoosing.js
@@ -112,9 +112,7 @@ export default function WordChoosing({ words }) {
         >
           Start Drawing!
         </button>
-      ) : (
-        <p>Waiting for host to start...</p>
-      )}
+      ) : null}
     </div>
   );
 }
