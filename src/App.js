81969b8d41ac9d9710611e31a421bde7fc2e1372
Kai Vandivier <kpvandivier@gmail.com>
2020-05-22 21:01:34 -0700

    Add `How to play`

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import styles from "./styles/Homepage.module.css";
 const ENDPOINT =
   process.env.NODE_ENV === "production" ? prodEndpoint : endpoint;
 const socket = socketIOClient(ENDPOINT);
+
 export const SocketContext = React.createContext(null);
 export const RoomContext = React.createContext(null);
 
@@ -56,6 +57,65 @@ function App() {
         ) : (
           <div className={`${utilStyles.center} ${utilStyles.fullPage}`}>
             <h1 className={utilStyles.titleHome}>Pict-o-phone!</h1>
+
+            <details className={styles.details}>
+              <summary>How to Play</summary>
+              <div>
+                <p>
+                  The game is like a cross between Telephone and Pictionary: you
+                  will try to communicate a word or phrase all the way around
+                  the room through a chain of alternating drawings and guesses.
+                  Try to get your word to make it all the way around the room!
+                </p>
+                <p>The game will happen like this:</p>
+                <ol>
+                  <li>
+                    <p>
+                      When the room host starts the game, everyone will choose a
+                      word from a small list presented to them.
+                    </p>
+                  </li>
+                  <li>
+                    <p>
+                      Then, when the game starts, everyone will have 60 seconds
+                      to draw a picture that represents their word or phrase.
+                    </p>
+                  </li>
+                  <li>
+                    <p>
+                      When the drawing time is up, everyone's drawing will be
+                      passed to the next player, and everyone will have 20
+                      seconds to look at the drawing that was passed to them and
+                      try to guess the word or phrase the previous player was
+                      trying to represent.
+                    </p>
+                  </li>
+                  <li>
+                    <p>
+                      When the guessing time is up, everyone's guesses will be
+                      passed to the next player, and everyone will have 60
+                      seconds to draw a picture that represents the word or
+                      phrase that has just been passed to them from the previous
+                      player's guess.
+                    </p>
+                  </li>
+                  <li>
+                    <p>
+                      The drawing and guessing will repeat until the drawings
+                      have made it all the way around the room, at which point
+                      the the game will end, and a replay will show the journey
+                      of all of the words and drawings!
+                    </p>
+                  </li>
+                </ol>
+              </div>
+              <Button
+                textButton
+                onClick={(e) => (e.target.parentElement.open = false)}
+              >
+                Close Instructions
+              </Button>
+            </details>
 
             <form
               autoComplete="off"
