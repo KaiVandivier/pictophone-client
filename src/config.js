8b1f64e8bac2ea047cc347a5165555b546f27114
Kai Vandivier <kpvandivier@gmail.com>
2020-09-07 11:57:44 -0700

    fix: change dev endpoint

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-const endpoint = "http://10.0.0.250:4000";
+const endpoint = "http://localhost:4000";
 const prodEndpoint = "https://pictophone-server.herokuapp.com/";
 
 module.exports = { endpoint, prodEndpoint };
